b61fe5082a9a4bd0b21400f7685fe594ba2c65dc
doma17 <rovin1273@gmail.com>
2024-04-01 19:45:51 +0900

    Initial commit

---
+++ src/main/java/inu/spp/cryptocoffee/CryptoCoffeeApplication.java
@@ -0,0 +1,13 @@
+package inu.spp.cryptocoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCoffeeApplication.class, args);
+    }
+
+}

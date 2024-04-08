b8e384704cd0ca8b8f9f53bc245fefaeb6d0813b
doma17 <rovin1273@gmail.com>
2024-04-08 21:20:20 +0900

    feat : CompanyEntity 추가
    회사 정보 저장 엔티티

---
+++ src/main/java/inu/spp/cryptocoffee/domain/entity/CompanyEntity.java
@@ -0,0 +1,27 @@
+package inu.spp.cryptocoffee.domain.entity;
+
+import inu.spp.cryptocoffee.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "company")
+public class CompanyEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long companyId;
+
+    private String name;
+
+    @Column(name = "number")
+    private String companyNumber;
+
+    @Embedded
+    private Address address;
+}

b3c30873e4d7d8f5f5ede8b165db8e8b3ebb7de0
doma17 <rovin1273@gmail.com>
2024-04-09 19:10:03 +0900

    comment : Swagger Schema 및 주석 추가 2

--- src/main/java/inu/spp/cryptocoffee/domain/dto/ItemRequestDto.java
+++ src/main/java/inu/spp/cryptocoffee/domain/dto/ItemRequestDto.java
@@ -1,12 +1,22 @@
 package inu.spp.cryptocoffee.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ItemRequestDto {
+    @NotEmpty(message = "상품명을 입력해주세요.")
+    @Schema(description = "상품명", example = "아메리카노")
     private String name;
+
+    @NotEmpty(message = "상품 설명을 입력해주세요.")
+    @Schema(description = "상품 설명", example = "이 커피는 맛있다")
     private String description;
-    private Long categoryId;
+
+    @NotEmpty(message = "상품 카테고리를 입력해주세요.")
+    @Schema(description = "상품 카테고리", example = "커피")
+    private String categoryName;
 }

a52286d53f415664341bbb127d321ab5f10b4532
doma17 <rovin1273@gmail.com>
2024-04-11 20:27:53 +0900

    feat : SecurityConfig 추가

---
+++ src/main/java/inu/spp/cryptocoffee/global/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package inu.spp.cryptocoffee.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        // From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        // http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        // 권한 설정
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+}

f4ba5df971677be1b232763f823a942060049221
doma17 <rovin1273@gmail.com>
2024-04-17 23:39:55 +0900

    feat : JWT 기능 추가 #1
    JwtFilter : Jwt Token 필터링 기능
    JwtUtil : Jwt Token 생성 및 검증 기능
    LoginFilter : 로그인 Authentication 기능

---
+++ src/main/java/inu/spp/cryptocoffee/user/jwt/JwtFilter.java
@@ -0,0 +1,72 @@
+package inu.spp.cryptocoffee.user.jwt;
+
+import inu.spp.cryptocoffee.user.dto.CustomUserDetails;
+import inu.spp.cryptocoffee.user.entity.UserEntity;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+
+        //Authorization 헤더 검증
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            log.info("[doFilterInternal] authorization not exist");
+            filterChain.doFilter(request, response);
+            //조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        log.info("[doFilterInternal] authorization now");
+        String token = authorization.split(" ")[1];
+
+        //토큰 소멸 시간 검증
+        if (jwtUtil.isExpired(token)) {
+
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+
+            //조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        //토큰에서 username과 role 획득
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        //userEntity를 생성하여 값 set
+        UserEntity userEntity = UserEntity.builder()
+                .username(username)
+                .password("temppassword")
+                .role(role)
+                .build();
+
+        //UserDetails에 회원 정보 객체 담기
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        //스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        //세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}

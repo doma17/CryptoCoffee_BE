a7ede0489081d5009965197a7649eb68a0804cfc
doma17 <rovin1273@gmail.com>
2024-04-17 23:42:38 +0900

    feat : SecurityConfig 작성 close #1
    1. csrf disable
    2. From 로그인 방식 disable
    3. http basic 인증 방식 disable
    4. Session -> Stateless
    5. CORS 세팅

--- src/main/java/inu/spp/cryptocoffee/global/config/SecurityConfig.java
+++ src/main/java/inu/spp/cryptocoffee/global/config/SecurityConfig.java
@@ -1,20 +1,51 @@
 package inu.spp.cryptocoffee.global.config;
 
+import inu.spp.cryptocoffee.user.jwt.JwtFilter;
+import inu.spp.cryptocoffee.user.jwt.JwtUtil;
+import inu.spp.cryptocoffee.user.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtUtil jwtUtil) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         // csrf disable
+        // Jwt 방식에서는 Session을 STATLESS 방식으로 관리하기에 csrf 보안이 필요없다.
         http
                 .csrf((auth) -> auth.disable());
 
@@ -26,17 +57,48 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
-        // 권한 설정
+        // 경로별 인가 작업
         http
-                .authorizeHttpRequests(auth -> auth
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().permitAll());
 
-        //세션 설정
+        // 세션 설정
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        // 필터 추가 LoginFilter()는 인자를 받음
+        // (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+                        UsernamePasswordAuthenticationFilter.class);
+
+        // LoginFilter 앞에 JwtFilter를 추가
+        http
+                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+
+        // CORS Setting
+        http
+                .cors(cors -> cors.configurationSource(apiConfigurationSource()));
+
         return http.build();
     }
 
+    /**
+     * CORS Setting Method
+     */
+    public CorsConfigurationSource apiConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("https://api.example.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST")); // GET, POST 방식만 지원
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 서버 내의 모든 URL에 대해서 허용
+
+        return source;
+    }
 }

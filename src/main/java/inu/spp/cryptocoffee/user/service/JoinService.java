4e53b04125301b45097346db521f7e4c02365d32
doma17 <rovin1273@gmail.com>
2024-04-17 23:40:13 +0900

    feat : User Join 도메인 기능 추가 #1

---
+++ src/main/java/inu/spp/cryptocoffee/user/service/JoinService.java
@@ -0,0 +1,47 @@
+package inu.spp.cryptocoffee.user.service;
+
+import inu.spp.cryptocoffee.email.entity.CompanyEntity;
+import inu.spp.cryptocoffee.email.repository.CompanyRepository;
+import inu.spp.cryptocoffee.user.dto.JoinDto;
+import inu.spp.cryptocoffee.user.entity.UserEntity;
+import inu.spp.cryptocoffee.user.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, CompanyRepository companyRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDto joinDto) {
+
+        String username = joinDto.getUsername();
+        String password = joinDto.getPassword();
+        String company = joinDto.getCompany();
+
+        if (userRepository.existsByUsername(username)) {
+            return;
+        }
+
+        CompanyEntity companyEntity = companyRepository.findByName(company).orElseThrow(()
+                -> new IllegalArgumentException("존재하지 않는 회사명입니다."));
+
+        UserEntity data = UserEntity.builder()
+                .username(username)
+                .password(bCryptPasswordEncoder.encode(password))
+                .name(joinDto.getName())
+                .company(companyEntity)
+                .role("ROLE_ADMIN")
+                .build();
+
+        userRepository.save(data);
+    }
+}

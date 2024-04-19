7992f80cf26f50af98aeb596eca334935338ab6a
doma17 <rovin1273@gmail.com>
2024-04-19 21:48:10 +0900

    feat : SecurityConfig 수정 close #9
    1. CustomLoginFilter, CustomLogoutFilter 적용
    2. RefreshTokenRepository, JwtTimeComponent DI 추가
    3. "/reissue" 도메인 허가

--- src/main/java/inu/spp/cryptocoffee/global/config/SecurityConfig.java
+++ src/main/java/inu/spp/cryptocoffee/global/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package inu.spp.cryptocoffee.global.config;
 
-import inu.spp.cryptocoffee.user.jwt.JwtAuthenticationFilter;
-import inu.spp.cryptocoffee.user.jwt.JwtUtil;
-import inu.spp.cryptocoffee.user.jwt.LoginFilter;
+import inu.spp.cryptocoffee.user.jwt.*;
+import inu.spp.cryptocoffee.user.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,23 +13,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final RefreshTokenRepository refreshTokenRepository;
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JwtUtil jwtUtil;
-
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtUtil jwtUtil) {
-        this.authenticationConfiguration = authenticationConfiguration;
-        this.jwtUtil = jwtUtil;
-    }
+    private final JwtTimeComponent jwtTimeComponent;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
@@ -61,6 +60,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/reissue").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().permitAll());
 
@@ -72,12 +72,13 @@ public class SecurityConfig {
         // 필터 추가 LoginFilter()는 인자를 받음
         // (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
-                        UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new CustomLoginFilter(authenticationManager(authenticationConfiguration), refreshTokenRepository, jwtUtil, jwtTimeComponent), UsernamePasswordAuthenticationFilter.class);
 
         // LoginFilter 앞에 JwtFilter를 추가
         http
-                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), LoginFilter.class);
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), CustomLoginFilter.class);
+        http
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshTokenRepository), LogoutFilter.class);
 
         // CORS Setting
         http

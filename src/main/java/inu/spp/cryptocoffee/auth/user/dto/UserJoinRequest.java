e6c0047b605c5bed4f83d118739fc3278e98d705
kbm <rovin1273@gmail.com>
2024-05-20 22:28:04 +0900

    chore : UserJoinRequestDto에 Swagger Schema 추가

--- src/main/java/inu/spp/cryptocoffee/auth/user/dto/UserJoinRequest.java
+++ src/main/java/inu/spp/cryptocoffee/auth/user/dto/UserJoinRequest.java
@@ -9,19 +9,23 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserJoinRequest {
+
+    @Schema(description = "유저의 아이디", example = "test")
     @NotNull(message = "username is null")
     @NotEmpty(message = "username is empty")
-    @Schema(description = "유저의 아이디", example = "test")
     private String username;
 
+    @Schema(description = "유저의 비밀번호", example = "test1234")
     @NotNull(message = "password is null")
     @NotEmpty(message = "password is empty")
     private String password;
 
+    @Schema(description = "유저의 이름", example = "홍길동")
     @NotNull(message = "name is null")
     @NotEmpty(message = "name is empty")
     private String name;
 
+    @Schema(description = "유저의 회사", example = "CrpytoCoffee")
     @NotNull(message = "company is null")
     @NotEmpty(message = "company is empty")
     private String company;

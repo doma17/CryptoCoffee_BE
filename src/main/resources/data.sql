4058413789ac1de414d505b2d7e22da6d2100da2
kbm <rovin1273@gmail.com>
2024-05-25 23:53:28 +0900

    chore : data.sql 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,4 +9,12 @@ INSERT INTO crypto_coffee.company (created_at, id, updated_at, city, name, numbe
 INSERT INTO crypto_coffee.company (created_at, id, updated_at, city, name, number, street, zipcode) VALUES ('2024-05-20 22:37:51.789260', 3, '2024-05-20 22:37:51.789260', '인천', '인천대학교', '123-45-33333', '아카데미로 119', '119119');
 INSERT INTO crypto_coffee.company (created_at, id, updated_at, city, name, number, street, zipcode) VALUES ('2024-05-20 22:41:15.787826', 4, '2024-05-20 22:41:15.787826', '인천', 'CryptoCoffee', '123-45-67890', '아카데미로 119', '12345');
 
-#
+#ITEM
+INSERT INTO crypto_coffee.item (category_id, created_at, id, updated_at, description, name, price) VALUES (1, '2024-05-25 23:46:28.846987', 7, '2024-05-25 23:46:28.846987', '카페 라떼다', '카페 라떼', 2500);
+INSERT INTO crypto_coffee.item (category_id, created_at, id, updated_at, description, name, price) VALUES (2, '2024-05-25 23:47:02.639875', 8, '2024-05-25 23:47:02.639875', '달고 달디단 허니밀크티', '허니 밀크티', 1800);
+INSERT INTO crypto_coffee.item (category_id, created_at, id, updated_at, description, name, price) VALUES (3, '2024-05-25 23:47:30.340850', 9, '2024-05-25 23:47:30.340850', '달디 달고 시고 신 허니 유자', '허니 유자', 2200);
+
+#CATEGORY
+INSERT INTO crypto_coffee.category (created_at, id, updated_at, name) VALUES ('2024-05-25 23:34:05.990254', 1, '2024-05-25 23:34:05.990254', '커피');
+INSERT INTO crypto_coffee.category (created_at, id, updated_at, name) VALUES ('2024-05-25 23:34:13.049885', 2, '2024-05-25 23:34:13.049885', '논커피');
+INSERT INTO crypto_coffee.category (created_at, id, updated_at, name) VALUES ('2024-05-25 23:34:26.198815', 3, '2024-05-25 23:34:26.198815', '티/에이드');
